66922929fe98fb9a0ca060649fa62c6c62692d88
sweetpoisonxx <avilsu111@gmail.com>
2023-02-13 11:07:34 +0530

    third commit

--- src/section/calorie_calculator/Calorie_calculator.js
+++ src/section/calorie_calculator/Calorie_calculator.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 import "./calorie_calculator_style.css"
-import { service } from "../../service/service";
-const Calorie_calulator = ()=>{
+import fetchdata from "../../service/service.js";
+const Calorie_calulator = (props)=>{
+    useEffect(() => {
+        clickhandler()
+           },[]);
+const [food,newfood]=useState("");
+const [fooddata,newfooddata]=useState('');
+    const changehandler = (event)=>{
+        newfood(event.target.value)
+        // console.log(food)
+}
 
+const clickhandler=()=>{
+ fetchdata(food).then((response)=>{newfooddata(response)})
+}
     return(
         <div className="Box">
-            <input></input>
-            <h1>Hello HERE WE START</h1>
+            <div>
+            <input placeholder="input food" onChange={changehandler}></input>
+            <button onClick={clickhandler}>Search</button>
+            </div>
+            <div>
+                <h2>Calorie Data</h2>
+                <h2>Energy: {fooddata}</h2>
+            </div>
         </div>
     );
 }
--- src/service/service.js
+++ src/service/service.js
@@ -1,4 +1,8 @@
-const service =(value)=>{
 
+   
+   const fetchdata=async(dataquery)=>{
+   const data=await fetch(`https://api.edamam.com/api/nutrition-data?app_id=c7b01f33&app_key=5aafc6e31997be74adbf9e539c4bc8c2&nutrition-type=logging&ingr=${dataquery}`); 
+    const response=await data.json();
+    return response.totalNutrientsKCal.ENERC_KCAL.quantity;
 };
-export default service;
+export default fetchdata;
